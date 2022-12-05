a62e9812dad9b39bde8fff9ffeed3e6de28ad7a8
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-12-05 17:52:26 +0100

    First partial implementation for Polygon::fill

--- src/shapes/polygon.rs
+++ src/shapes/polygon.rs
@@ -10,6 +10,23 @@ pub struct Polygon {
     color: Color,
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Edge {
+    pub y2: f64,
+    pub x: f64,
+    pub delta: f64,
+}
+
+impl Edge {
+    fn from(first: &Vec2d, next: &Vec2d) -> Self {
+        Self {
+            y2: next.y,
+            x: first.x,
+            delta: (next.x - first.x) / (next.y - first.y),
+        }
+    }
+}
+
 impl Polygon {
     /// Create a new polygon with the specified points.
     pub fn from_points(points: Vec<Vec2d>) -> Self {
@@ -28,12 +45,57 @@ impl Polygon {
     pub fn set_color(&mut self, color: Color) {
         self.color = color;
     }
+
+    fn fill(&self, img: &mut Image) {
+        let HEIGHT = img.rows();
+        let WIDTH = img.cols();
+
+        let mut edge_table: Vec<Vec<Edge>> = vec![vec![]];
+
+        for i in 0..HEIGHT {
+            let mut edges = vec![];
+
+            for (j, current) in self.points.iter().enumerate() {
+                if current.x as usize == i {
+                    let prev = self.points[if j > 0 { j - 1 } else { self.points.len() - 1 }];
+                    let next = self.points[if j < self.points.len() - 1 { j + 1 } else { 0 }];
+
+                    if current.y < prev.y {
+                        edges.push(Edge::from(&current, &prev));
+                    }
+
+                    if current.y < next.y {
+                        edges.push(Edge::from(&current, &prev));
+                    }
+                }
+            }
+
+            edge_table.push(edges);
+        }
+
+        let mut active_edges: Vec<Edge> = vec![];
+
+        for y in 0..HEIGHT {
+            active_edges = active_edges
+                .into_iter()
+                .filter(|edge| edge.y2 as usize <= y)
+                .collect();
+
+            edge_table[y]
+                .iter()
+                .for_each(|elem| active_edges.push(*elem));
+
+            active_edges.sort_by(|first, second| first.x.partial_cmp(&second.x).unwrap());
+            // TODO: Implement me pls! :<
+            todo!();
+        }
+    }
 }
 
 impl Shape for Polygon {
     fn draw(&self, img: &mut Image) {
         if self.filled {
-            todo!();
+            self.fill(img);
         }
 
         for (i, point) in self.points.iter().enumerate() {
