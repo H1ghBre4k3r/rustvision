8bb16ab698f5bd89e46bdc04227a00f9e03c73b9
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-12-06 20:22:53 +0100

    Add documentation for rgb macro

--- src/color.rs
+++ src/color.rs
@@ -38,6 +38,37 @@ impl PNM for Color {
     }
 }
 
+/// Utility macro for creating an RGB color.
+///
+/// # Examples
+///
+/// You can create a default (i.e., black) color by calling this macro without any arguments:
+///
+/// ```rust
+/// use rustvision::{rgb, color::Color};
+///
+/// let color = rgb!();
+/// assert_eq!(color, Color { r: 0, g: 0, b: 0});
+/// ```
+///
+/// If you want to create a color with a certain gray value (i.e., all channels have the same
+/// value), you can do so by calling the macro with a single argument:
+///
+/// ```rust
+/// use rustvision::{rgb, color::Color};
+///
+/// let color = rgb!(42);
+/// assert_eq!(color, Color { r: 42, g: 42, b: 42});
+/// ```
+///
+/// By calling the macro with three arguments, you create a simple color:
+///
+/// ```rust
+/// use rustvision::{rgb, color::Color};
+///
+/// let color = rgb!(42, 17, 129);
+/// assert_eq!(color, Color { r: 42, g: 17, b: 129});
+/// ```
 #[macro_export]
 macro_rules! rgb {
     () => {
